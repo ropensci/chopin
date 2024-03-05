87259e0f377aff562fa5497a772d30cab26e9c8e
Insang Song <insang.song@nih.gov>
2024-03-05 13:33:24 -0500

    vignette cont'd

---
+++ tools/par_compare.r
@@ -0,0 +1,139 @@
+# Load the required packages
+library(doParallel)
+library(foreach)
+library(mirai)
+
+setwd("/Volumes/songi2/projects/chopin/")
+
+# Set the number of cores to use
+# num_cores <- 16L
+# # Register the parallel backend
+# doParallel::registerDoParallel(cores = num_cores)
+
+# Define the function to be executed in parallel
+popplace <- readRDS("./input/populated_place_2022.rds")
+bils <- list.files("input", "bil$", recursive = TRUE, full.names = TRUE)
+bilssds <- terra::rast(bils[-13])
+
+
+cl <- mirai::make_cluster(4L)
+
+tictoc::tic()
+doch <-
+mirai::mirai(
+  .expr = {
+    pkgs <-
+      c("chopin", "terra", "stars", "sf", "future", "doFuture", "parallelly", "tigris", "exactextractr", "tictoc")
+    invisible(sapply(pkgs, library, character.only = TRUE, quietly = TRUE))
+    options(tigris_use_cache = TRUE, sf_use_s2 = FALSE)
+    bils <- list.files("input", "bil$", recursive = TRUE, full.names = TRUE)
+    bilssds <- terra::rast(bils[-13])
+    feature <- readRDS("./input/populated_place_2022.rds")
+    ll <- vector("list", length = length(states))
+
+    for (k in seq_along(states)) {
+        ppthis <- feature[feature$STATEFP == states[k], ]
+        ppbuf <- terra::buffer(terra::vect(ppthis), width = 1e4)
+        ppbuf <- sf::st_as_sf(ppbuf)
+
+        ll[[k]] <-
+        exactextractr::exact_extract(
+            bilssds,
+            ppbuf,
+            fun = "mean",
+            stack_apply = TRUE,
+            force_df = TRUE,
+            append_cols = "GEOID",
+            max_cells_in_memory = 2.14e9
+        )
+
+    }
+
+    return(ll)
+
+  },
+    states = unique(popplace$STATEFP)
+  
+)
+
+mirai::call_mirai(doch) -> dochcal
+tictoc::toc()
+mirai::status(cl)
+
+# 16: 407.5 sec
+# 4: 502.979 sec
+
+
+##
+library(furrr)
+library(future)
+library(doFuture)
+doFuture::registerDoFuture()
+plan(list(multisession), workers = 6L)
+
+
+tictoc::tic()
+fur_ll <-
+furrr::future_map(
+  .x = unique(popplace$STATEFP),
+  .f = function(x) {
+    pkgs <-
+      c("chopin", "terra", "stars", "sf", "future", "doFuture", "parallelly", "tigris", "exactextractr", "tictoc")
+    invisible(sapply(pkgs, library, character.only = TRUE, quietly = TRUE))
+    options(tigris_use_cache = TRUE, sf_use_s2 = FALSE)
+    bils <- list.files("input", "bil$", recursive = TRUE, full.names = TRUE)
+    bilssds <- terra::rast(bils[-13])
+    feature <- readRDS("./input/populated_place_2022.rds")
+
+      ppthis <- feature[feature$STATEFP == x, ]
+      ppbuf <- terra::buffer(terra::vect(ppthis), width = 1e4)
+      ppbuf <- sf::st_as_sf(ppbuf)
+
+        exactextractr::exact_extract(
+          bilssds,
+          ppbuf,
+          fun = "mean",
+          stack_apply = TRUE,
+          force_df = TRUE,
+          append_cols = "GEOID",
+          max_cells_in_memory = 2.14e9
+        )
+    }
+)
+tictoc::toc()
+# up to 900MB / thread
+# 120 sec
+
+# lapply
+library(future.apply)
+tictoc::tic()
+fur_lla <-
+future.apply::future_lapply(
+  X = unique(popplace$STATEFP),
+  FUN = function(x) {
+    pkgs <-
+      c("chopin", "terra", "stars", "sf", "future", "doFuture", "parallelly", "tigris", "exactextractr", "tictoc")
+    invisible(sapply(pkgs, library, character.only = TRUE, quietly = TRUE))
+    options(tigris_use_cache = TRUE, sf_use_s2 = FALSE)
+    bils <- list.files("input", "bil$", recursive = TRUE, full.names = TRUE)
+    bilssds <- terra::rast(bils[-13])
+    feature <- readRDS("./input/populated_place_2022.rds")
+
+      ppthis <- feature[feature$STATEFP == x, ]
+      ppbuf <- terra::buffer(terra::vect(ppthis), width = 1e4)
+      ppbuf <- sf::st_as_sf(ppbuf)
+
+        exactextractr::exact_extract(
+          bilssds,
+          ppbuf,
+          fun = "mean",
+          stack_apply = TRUE,
+          force_df = TRUE,
+          append_cols = "GEOID",
+          max_cells_in_memory = 2.14e9
+        )
+    }
+)
+tictoc::toc()
+# 114.906 sec
+# 700 MB / thread
