85206885d3f6c668d489dbeb557ccbec22635509
Insang Song <insang.song@nih.gov>
2024-08-09 16:34:06 -0400

    0.7.8 - Late reflection of .check_package refactoring - Logo file replaced - Test coverage YAML is set to upload artifacts with covr::report() output - README rebuilt

--- tests/testthat/test-processing.R
+++ tests/testthat/test-processing.R
@@ -121,7 +121,64 @@ testthat::test_that("Character input works", {
 
 })
 
-testthat::test_that("summarize_aw works as expected.", {
+
+
+testthat::test_that("summarize_aw works -- character-character", {
+  withr::local_package("sf")
+  withr::local_package("terra")
+  withr::local_package("units")
+  withr::local_package("dplyr")
+  withr::local_package("testthat")
+  withr::local_options(list(sf_use_s2 = FALSE))
+
+  ncpath <- system.file("shape/nc.shp", package = "sf")
+  nc <- sf::st_read(ncpath)
+  nc <- sf::st_transform(nc, 5070)
+  pp <- sf::st_sample(nc, size = 300)
+  pp <- sf::st_as_sf(pp)
+  pp[["id"]] <- seq(1, nrow(pp))
+  sf::st_crs(pp) <- "EPSG:5070"
+  flds <- c("BIR74", "SID74", "BIR79", "SID79")
+  ppb <- sf::st_buffer(pp, nQuadSegs = 180, dist = units::set_units(20, "km"))
+  ppbpath <- file.path(tempdir(), "ppb.shp")
+  sf::st_write(ppb, ppbpath, append = FALSE)
+
+  system.time({
+    ppb_nc_aw <- summarize_aw(ppbpath, ncpath, target_fields = flds, "id")
+  })
+  testthat::expect_true(inherits(ppb_nc_aw, "data.frame"))
+
+})
+
+
+testthat::test_that("summarize_aw works -- sf-sf", {
+  withr::local_package("sf")
+  withr::local_package("terra")
+  withr::local_package("units")
+  withr::local_package("dplyr")
+  withr::local_package("testthat")
+  withr::local_options(list(sf_use_s2 = FALSE))
+
+  nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"))
+  nc <- sf::st_transform(nc, 5070)
+  pp <- sf::st_sample(nc, size = 300)
+  pp <- sf::st_as_sf(pp)
+  pp[["id"]] <- seq(1, nrow(pp))
+  sf::st_crs(pp) <- "EPSG:5070"
+  flds <- c("BIR74", "SID74", "BIR79", "SID79")
+  ppb <- sf::st_buffer(pp, nQuadSegs = 180, dist = units::set_units(20, "km"))
+
+  testthat::expect_warning(
+    system.time({
+      ppb_nc_aw <- summarize_aw(ppb, nc, target_fields = flds, "id")
+    }),
+    "st_interpolate_aw assumes attributes are constant or uniform over areas of x"
+  )
+  testthat::expect_true(inherits(ppb_nc_aw, "data.frame"))
+
+})
+
+testthat::test_that("summarize_aw works -- terra-terra", {
   withr::local_package("sf")
   withr::local_package("terra")
   withr::local_package("units")
@@ -156,10 +213,30 @@ testthat::test_that("summarize_aw works as expected.", {
   )
   testthat::expect_s3_class(ppb_nc_aw, "data.frame")
 
+})
+
+testthat::test_that("summarize_aw works -- error cases", {
+  withr::local_package("sf")
+  withr::local_package("terra")
+  withr::local_package("units")
+  withr::local_package("dplyr")
+  withr::local_package("testthat")
+  withr::local_options(list(sf_use_s2 = FALSE))
+
+  nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"))
+  nc <- sf::st_transform(nc, 5070)
+  pp <- sf::st_sample(nc, size = 300)
+  pp <- sf::st_as_sf(pp)
+  pp[["id"]] <- seq(1, nrow(pp))
+  sf::st_crs(pp) <- "EPSG:5070"
+  flds <- c("BIR74", "SID74", "BIR79", "SID79")
+  ppb <- sf::st_buffer(pp, nQuadSegs = 180, dist = units::set_units(20, "km"))
+
   # error case
   testthat::expect_error(summarize_aw(as.list(ppb_t), nc, fld, "id"))
   testthat::expect_error(summarize_aw(ppb_t, list(1, 3), fld, "id"))
 })
+
 
 
 testthat::test_that("SEDC are well calculated.", {
@@ -213,6 +290,52 @@ testthat::test_that("SEDC are well calculated.", {
   testthat::expect_warning(
     summarize_sedc(ncpnts2, ncrandsf, "pid", 3e4L, 5e4L, polnames)
   )
+})
+
+
+
+testthat::test_that("SEDC warning message with multiple fields overlapped", {
+  withr::local_package("sf")
+  withr::local_package("terra")
+  withr::local_package("dplyr")
+  withr::local_package("testthat")
+  withr::local_options(list(sf_use_s2 = FALSE))
+
+  # read and generate data
+  ncpath <- system.file("shape/nc.shp", package = "sf")
+  ncpoly <- terra::vect(ncpath) |>
+    terra::project("EPSG:5070")
+  ncpnts <-
+    readRDS(system.file("extdata/nc_random_point.rds", package = "chopin"))
+  ncpnts <- terra::vect(ncpnts)
+  ncpnts <- terra::project(ncpnts, "EPSG:5070")
+  ncrand <- terra::spatSample(ncpoly, 250L)
+  ncrand$pollutant1 <- stats::rgamma(250L, 1, 0.01)
+  ncrand$pollutant2 <- stats::rnorm(250L, 30, 4)
+  ncrand$pollutant3 <- stats::rbeta(250L, 0.5, 0.5)
+
+  polnames <- paste0("pollutant", 1:3)
+  testthat::expect_no_error(
+    summarize_sedc(ncpnts, ncrand, "pid", 3e4L, NULL, polnames)
+  )
+
+  ncpnts$overlap <- 1L
+  ncrand$overlap <- 100L
+
+  testthat::expect_warning(
+    sedc_calc <-
+      summarize_sedc(ncpnts, ncrand, "pid", 3e4L, 5e4L, polnames),
+    "There are 1 fields with the same name. The result may be inaccurate."
+  )
+  testthat::expect_s3_class(sedc_calc, "data.frame")
+
+  testthat::expect_equal(
+    sum(paste0(polnames, "_sedc") %in% names(sedc_calc)),
+    length(polnames)
+  )
+  testthat::expect_true(!is.null(attr(sedc_calc, "sedc_bandwidth")))
+  testthat::expect_true(!is.null(attr(sedc_calc, "sedc_threshold")))
+
 })
 
 testthat::test_that("Kernel functions work okay", {
